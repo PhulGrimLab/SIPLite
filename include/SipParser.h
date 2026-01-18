bf73c18844df102c46b0c5ef7847c666d0f2e1d4
PhulGrimLab <phulgrimlab@gmail.com>
2026-01-18 23:18:07 +0900

    주석 추가

--- include/SipParser.h
+++ include/SipParser.h
@@ -2,7 +2,7 @@
 
 #include <string>
 
-struct SipMessage;
+struct SipMessage;  // from SipCore.h
 
 // Parse raw SIP text into SipMessage. Returns true on success.
 // Implementation lives in src/SipParser.cpp to keep heavy parsing logic out of headers.
